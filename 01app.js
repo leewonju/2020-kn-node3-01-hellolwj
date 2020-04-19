174e73693ccd38cad66793dd40b070f90736e3ce
booldook <leewonju2006@naver.com>
2020-04-19 13:05:19 +0900

    app

---
+++ 01app.js
@@ -0,0 +1,15 @@
+const  express = require('express');
+const  app= express();
+
+app.listen(3000, function(){
+console.log("http://127.0.0.1:3000");
+});
+
+app.get("/",    function(req, res, next){
+res.send("<h1>hello, World</h1>")
+});
+
+app.get("/hello",    function(req, res, next){
+    var   name= req.query.name;
+    res.send("<h1>hello, "+name+"</h1>")
+    });
--- app.js
+++ app.js
@@ -1,1 +1,8 @@
+var  moment= require('moment');
 const a = 10;
+const  b =20;
+console.log(a+b);
+console.log(new  Date());
+console.log(moment(new Date()).format('YYYY-MM-DD HH:mm:ss'));
+
+
